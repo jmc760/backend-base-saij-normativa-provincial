4f6acbde627691a6e8e88369186db0d3003227db
Juan Manuel Cerdeira <juanmanuelcerdeira761@gmail.com>
2025-08-28 14:07:30 -0300

    initial commit

---
+++ src/main/java/com/jcerdeira/base_saij_normativa_provincial/BaseSaijNormativaProvincialApplication.java
@@ -0,0 +1,13 @@
+package com.jcerdeira.base_saij_normativa_provincial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseSaijNormativaProvincialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseSaijNormativaProvincialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jcerdeira/base_saij_normativa_provincial/BaseSaijNormativaProvincialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcerdeira.base_saij_normativa_provincial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseSaijNormativaProvincialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
